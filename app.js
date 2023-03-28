d30b52a4b440190952b8af827ba444cf5be00e80
DOMAA21 <clashecurry21@gmail.com>
2023-03-28 10:42:09 +0800

    haha

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const app = express();
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const auth = require("./routes/auth");
+const products = require("./routes/product");
+const errorMiddleware = require("./middlewares/error");
+const order = require('./routes/order');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
+
+
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true}));
+
+
+  
+app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
+app.use(express.json());
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cookieParser());
+app.use(fileUpload());
+
+app.use("/api/v1", products);
+app.use("/api/v1", auth);
+app.use('/api/v1', order);
+app.use(errorMiddleware);
+
+module.exports = app;
---
+++ utils/jwtToken.js
@@ -0,0 +1,45 @@
+// const sendToken = (user, statusCode, res) => {
+//   // Create Jwt token
+
+//   const token = user.getJwtToken();
+
+//   // Options for cookie
+
+//   const options = {
+//     expires: new Date(
+//       Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+//     ),
+
+//     httpOnly: true,
+//   };
+
+//   res.status(statusCode).cookie("token", token, options).json({
+//     success: true,
+//     token,
+//     user,
+//   });
+// };
+
+const sendToken = (user, statusCode, res) => {
+
+  // Create Jwt token
+  const token = user.getJwtToken();
+
+  // Options for cookie
+  const options = {
+      expires: new Date(
+          Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+      secure: true,
+      samesite: 'none',
+  }
+  res.status(statusCode).cookie('token', token, options).json({
+      success: true,
+      token,
+      user
+  })
+
+}
+
+module.exports = sendToken;
